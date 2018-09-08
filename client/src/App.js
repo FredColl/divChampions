ff398605efdec797059ef2b272a365233e199d18
frsc <fredrik.schersten@reforceinternational.com>
2018-09-08 12:12:59 +0200

    step guide

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 
 import Home from './routes/Home';
+import StepGuide from './routes/StepGuide';
 
 class App extends Component {
   render() {
@@ -12,10 +13,12 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <h1 className="App-title">Dividend Champions Calculator</h1>
+            <Link to="/">List</Link>
+            <Link to="/stepguide">Step Guide</Link>
           </header>
           <div>
             <Route exact path="/" component={Home} />
-            <Route exact path="/about" component={() => <h1>hej2</h1>} />
+            <Route exact path="/stepguide" component={StepGuide} />
           </div>
         </div>
       </Router>
---
+++ client/src/routes/StepGuide.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const StepGuide = () => {
+  return (
+    <div>
+      <p>
+        1. The list helps filtering out the companies with the most{" "}
+        <b>Quality</b>
+      </p>
+      <p>
+        2. Verify where the dividend comes from. Focus on the free cash flow and
+        not the EPS (divide the companies net income with the diluted numbers of
+        shares) to find the <b>Value</b>
+      </p>
+      <p>
+        3. Find the free cash flow on for example morningstar, divide the
+        dividends payed with that number. A good procentage is around 75% or
+        less, the company should not use all their FCF money to pay dividends or
+        even take morgages or savings to pay them (over 100%). Of cource there
+        are execeptions, circular market companies might need to, part of their
+        operation to do so, but still a warning flag.
+      </p>
+    </div>
+  );
+};
+
+export default StepGuide;
